8392a15df96b13e0afc6169c21059be264d8fea8
Vasiliy Kevroletin <kevroletin@gmail.com>
2018-01-06 17:32:22 +1000

    Correctly generate sample plate with holes

--- app/Main.hs
+++ app/Main.hs
@@ -21,22 +21,26 @@ b3 (Switch idx) = idx + 7
 vert (Switch idx) n = idx + n
 verts sw xs = map (vert sw) xs
 
+switch_width = 10
+switch_height = 5
+hole_width = 8
+hole_height = 12
+
 switch :: Vec -> Vec -> State (Int, [Vec], [(Vec, Vec)]) Switch
 switch centerPos (Vec ax ay az) = do
   (lastIdx, vertexes, holes) <- get
-  put (lastIdx + 8, vertexes ++ [t0, t1, t2, t3, b0, b1, b2, b3], holes)
+  put (lastIdx + 8
+      , vertexes ++ [t0, t1, t2, t3, b0, b1, b2, b3]
+      , holes ++ [hole])
   return (Switch lastIdx)
   where
     rotation = compose [rotate (vec 1 0 0) (deg ax)
                        , rotate (vec 0 1 0) (deg ay)
                        , rotate (vec 0 0 1) (deg az)]
-    move = xfm_point (xfm_mult rotation (translate centerPos))
-    w   = 10
-    hw  = w/2
-    wi  = 8
-    hwi = wi/2
-    h   = 5
-    hh  = h/2
+    move = xfm_point (xfm_mult (translate centerPos) rotation)
+    hw  = switch_width/2
+    hwi = hole_width/2
+    hh  = switch_height/2
     t0 = move $ vec (-hw) (-hw) hh
     t1 = move $ vec (-hw) hw    hh
     t2 = move $ vec hw    hw    hh
@@ -45,19 +49,11 @@ switch centerPos (Vec ax ay az) = do
     b1 = move $ vec (-hw) hw    (-hh)
     b2 = move $ vec hw    hw    (-hh)
     b3 = move $ vec hw    (-hw) (-hh)
+    hole = (centerPos, Vec ax ay az)
 
 vec_to_scad (Vec x y z) = "[" ++ (show x) ++ ", " ++ (show y) ++ ", " ++ (show z) ++ "]"
 
 switch_to_scad sw = sandwich $ verts sw [0, 1, 2, 3]
-
-connect_by_square t0 t1 t2 t3 = [
-  "polyhedron("
-  , "points=["
-  , intercalate ",\n" (fmap vec_to_scad [t0, t1, t2, t3])
-  , "],"
-  , "faces=["
-  ,   "[0, 1, 2, 3]," , "]);"
-  ]
 
 top_to_bottom = (+ 4)
 sandwich xs = [xs, reverse $ map top_to_bottom xs]
@@ -87,16 +83,36 @@ connect_near_wall_left_right l r = [[t3 l, t0 r, b0 r, b3 l]]
 
 connect_far_wall_left_right l r = [[t1 r, t2 l, b2 l, b1 r]]
 
+render_polyhedron verts connections = [
+  "polyhedron("
+  , "points=["
+  , intercalate ",\n" (fmap vec_to_scad verts)
+  , "],"
+  , "faces=["
+  , intercalate ",\n" (fmap show connections)
+  , "]);"
+  ]
+
+hole_to_scad (Vec x y z, Vec ax ay az) = [
+  "translate(" ++ show [x, y, z] ++ ")"
+  , "rotate(" ++ show [ax, ay, az] ++ ")"
+  , "translate(" ++ show [-hw, -hw, -hh] ++ ")"
+  , "cube(" ++ show [hole_width, hole_width, hole_height] ++ ");"
+  ]
+  where
+    hw = hole_width / 2
+    hh = hole_height / 2
+
+render_holes holes = concat (map hole_to_scad holes)
+
+render_difference xs ys =
+  "difference() {" : xs ++ ys ++ ["};"]
+
 solve :: [String]
 solve = let (connections, (_, verts, holes)) = runState body (0, [], [])
-        in ["polyhedron("
-           , "points=["
-           , intercalate ",\n" (fmap vec_to_scad verts)
-           , "],"
-           , "faces=["
-           , intercalate ",\n" (fmap show connections)
-           , "]);"
-           ]
+        in render_difference
+             (render_polyhedron verts connections)
+             (render_holes holes)
   where
     body = do s0 <- switch (vec 0  0  0    ) (vec 0 0 0)
               s1 <- switch (vec 0  20 0    ) (vec 10 0 0)
