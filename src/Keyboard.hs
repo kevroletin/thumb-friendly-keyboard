cd322316769c68ad59c44d03b1a32a3efd9b7103
Vasiliy Kevroletin <kevroletin@gmail.com>
2018-02-21 14:43:50 +1000

    Add hight level project description

---
+++ src/Keyboard.hs
@@ -0,0 +1,46 @@
+-- | = Plate generator for 3d split keyboard.
+--
+-- A __plate__ is surface with sockets for key switches. This generator produces
+-- plates like this /(for Cherry MX switches)/:
+--
+-- <<doc/img/main_plate_half.png Plate>>
+--
+-- __\"3d\"__ means that a plate is a curved figure (as opposed to a "flat" plate
+-- made from a metal sheet). Generator produces plate using manually created
+-- config of sockets positions and angles.
+--
+-- __Split__ keyboard means that keyboard consists of two pars for left and right
+-- hand. Although currently it makes no difference because this module generates
+-- rectangle plates which can be used both for split and ordinary keyboards.
+--
+-- == Why
+--
+-- This generator produces Open Scad programs. Open Scad is a programming
+-- language itself but it has one big limitation. Open Scad provides no way to
+-- calculate point location after series of transformations. This is stopper for
+-- such project because coordinates are required to connect manually placed
+-- sockets.
+--
+-- There are two main objects in this module:
+--
+--     * "Parts" - keyboard parts like plate, keycaps, etc.
+--     * "Scad" - representation of OpenScad programs. It contains base primitives
+--     from OpenScad language such as cube, sphere, hull, etc. and more complicated
+--     composite figures as SphereLine and others.
+--
+-- General idea is that Parts are composed and transformed into Scad. Please note
+-- that both Parts and Scad objects can be transformed (translated or rotated).
+-- However usually it is better to transform Parts because it is possible to get
+-- coordinates out of transformed Part.
+--
+-- == Conventions
+--
+-- Implementation of this module uses several naming conventions.
+--
+--     * build* functions produce scad programs
+--     * we assume next view location /(this is default view location in OpenScad)/:
+--
+--         * x goes from left to right
+--         * y goes from "front" to "back"
+
+module Keyboard where
